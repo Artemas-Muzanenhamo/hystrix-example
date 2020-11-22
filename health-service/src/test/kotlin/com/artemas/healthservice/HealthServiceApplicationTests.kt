1d5dc6fc36eabf0b3ccce47d24396fc66d5d496c
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2020-11-22 15:03:44 +0000

    first commit

---
+++ health-service/src/test/kotlin/com/artemas/healthservice/HealthServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.artemas.healthservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HealthServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
